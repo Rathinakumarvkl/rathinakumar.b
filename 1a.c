1252a4e761894470f2855abd4239ba79debfb45a
Rathinakumarvkl <30342085+Rathinakumarvkl@users.noreply.github.com>
2017-07-22 16:46:28 +0530

    Rename 1.c to 1a.c

---
+++ 1a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number();
+clrscr;
+scanf("%d",&number);
+if(number >0)
+printf("%d",is a positive number\n, number);
+else
+print("%d",is a negative number\n,number);
+}
